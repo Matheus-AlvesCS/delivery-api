31cf1c49de4b1a859fbe13359bf9c40e7b6966a1
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-04-22 11:31:56 -0300

    feat(users): implement user registration with password hashing and validation, also prisma connection to db

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,39 @@
 import type { Request, Response } from "express"
 
+import z from "zod"
+import { hash } from "bcrypt"
+
+import { prisma } from "../database/prisma"
+import { AppError } from "../utils/app-error"
+
 export class UsersController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Ok!" })
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.email(),
+      password: z.string().trim().min(6),
+    })
+
+    const { email, name, password } = bodySchema.parse(request.body)
+
+    const existingUser = await prisma.user.findUnique({ where: { email } })
+
+    if (existingUser) {
+      throw new AppError("This email is already in use")
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+    })
+
+    const { password: _, ...userWithoutPassword } = user
+
+    return response.json(userWithoutPassword)
   }
 }
